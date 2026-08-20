7fe5dedbcf78e95576f259c26860a22e4d5ad47f
ruthwikreddyp-hue <ruthwikreddy.p@gmail.com>
2026-08-20 13:55:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='project20-v29';
+const CACHE='project20-v30';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('project20-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
